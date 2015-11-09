5135f228186c038ef1b3c57c41a5b58cf474501b
Vinicius Woloszyn <vinicius@open.inf.br>
2015-11-09 15:27:39 -0200

    setup

--- setup.py
+++ setup.py
@@ -1,6 +1,22 @@
 
-from pip.req import parse_requirements
+#!/usr/bin/env python
+#
+# Setup script for the Natural Language Toolkit
+#
+# Copyright (C) 2001-2015 NLTK Project
+# Author: Steven Bird <stevenbird1@gmail.com>
+#         Edward Loper <edloper@gmail.com>
+#         Ewan Klein <ewan@inf.ed.ac.uk>
+# URL: <http://nltk.org/>
+# For license information, see LICENSE.TXT
 
+# Work around mbcs bug in distutils.
+# http://bugs.python.org/issue10945
+
+import os
+
+# setuptools
+from setuptools import setup, find_packages
 setup(name='PyLinguistics',
       version='1.0',
       description='Python Distribution Utilities',
