b415932d397389a712671735126fc73b666122d4
Gustavo Gawryszewski <gustavo@gawry.com>
2020-04-10 17:35:13 -0300

    fix nos requirements

--- setup.py
+++ setup.py
@@ -17,7 +17,10 @@ import os
 
 # setuptools
 from setuptools import setup, find_packages
-from pip.req import parse_requirements
+try: # for pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError: # for pip <= 9.0.3
+    from pip.req import parse_requirements
 
 with open('requirements.txt') as f:
     required = f.read().splitlines()
